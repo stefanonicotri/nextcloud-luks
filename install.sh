996aef7aec93965bd1655dd038f52a702c0e324f
Stefano Nicotri <stefanonicotri@gmail.com>
2018-10-08 17:55:27 +0200

    #3

--- install.sh
+++ install.sh
@@ -3,12 +3,11 @@
 #check if it's debian
 
 SERVER_IP=$(hostname -I)
-#FQDN=`host $IP  | awk '{ print $NF }' | sed 's/.$//')`
 
 apt update 
 apt -y full-upgrade
 apt -y autoremove
-apt -y install pwgen git apt-transport-https ca-certificates curl gnupg2 software-properties-common
+apt -y install pwgen host git apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt update
@@ -45,5 +44,6 @@ cp docker-compose.yml /opt/nextcloud/
 cd /opt/nextcloud
 docker-compose up -d
 
+#FQDN=`host $IP  | awk '{ print $NF }' | sed 's/.$//')`
 #sed "/'localhost'/ s/$/ 1 => '$FQDN',/" /opt/nextcloud/data/config/config.php
 
